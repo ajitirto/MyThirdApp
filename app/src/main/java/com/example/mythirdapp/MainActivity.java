47220c16ed97e8e57efa61a3319e0ee56b536782
ajitirto <ajiwoke@gmail.com>
2023-08-03 13:59:46 +0700

    first app android

--- app/src/main/java/com/example/mythirdapp/MainActivity.java
+++ app/src/main/java/com/example/mythirdapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.mythirdapp;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
